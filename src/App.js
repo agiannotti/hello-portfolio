fc574f3142b7958de05fb895f14e71b1ee236ac0
tonyg <aj.gianno@gmail.com>
2021-05-03 20:41:19 -0500

    mat ui template

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
+import Chart from './Components/Dashboard/Chart';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Deposits from './Components/Dashboard/Deposits';
+import Orders from './Components/Dashboard/Orders';
+import Title from './Components/Dashboard/Title';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Chart />
+      <Dashboard />
+      <Deposits />
+      <Orders />
+      <Title />
+    </div>
+  );
 }
 
 export default App;
