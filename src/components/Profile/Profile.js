9665e6f006208f6bc12c96b6bf9fde515fd79a08
tonyg <aj.gianno@gmail.com>
2021-05-05 15:24:25 -0500

    timeline css

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,8 @@
 import { Typography } from '@material-ui/core';
 import React from 'react';
+import './Profile.css';
+import face from './../../assets/images/soho-self-square.png';
+import BasicTimeline from '../Timeline/Timeline';
 
 const Profile = () => {
   return (
@@ -9,11 +12,11 @@ const Profile = () => {
         <Typography className='title'>Giannotti</Typography>
       </div>
       <figure className='profile_image'>
-        <img src='' alt='' />
+        <img src={face} alt='face' />
       </figure>
 
       <div className='profile_information'>
-        Insert Timeline
+        <BasicTimeline />
         <br />
         <button>dat button</button>
       </div>
--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,7 +1,50 @@
 import React from 'react';
+import Timeline from '@material-ui/lab/Timeline';
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+import TimelineDot from '@material-ui/lab/TimelineDot';
+import WorkTwoToneIcon from '@material-ui/icons/WorkTwoTone';
 
-const Timeline = () => {
-  return <div>Hello, Timeline</div>;
+import './Timeline.css';
+import { Typography } from '@material-ui/core';
+
+const BasicTimeline = ({ title, children }) => {
+  return (
+    <Timeline className={'timeline'}>
+      {/*  Item Header */}
+      <TimelineItem className='timeline_firstItem'>
+        <TimelineSeparator>
+          <TimelineDot className={'timeline_dot_header'}>
+            <WorkTwoToneIcon style={{ width: 50, height: 50 }} />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+          <Typography variant='h6' className={'timeline_header'}>
+            {'title'}
+            {children}
+          </Typography>
+        </TimelineContent>
+      </TimelineItem>
+
+      {/* Remaining Items */}
+      <TimelineItem>
+        <CustomTimelineSeparator />
+        <TimelineContent>Code</TimelineContent>
+      </TimelineItem>
+    </Timeline>
+  );
 };
 
-export default Timeline;
+export const CustomTimelineSeparator = () => {
+  return (
+    <TimelineSeparator className={'separator_padding'}>
+      <TimelineDot variant={'outlined'} className={'timeline_dot'} />
+      <TimelineConnector />
+    </TimelineSeparator>
+  );
+};
+
+export default BasicTimeline;
