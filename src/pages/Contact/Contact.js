a3466c827002a79713f5feebf6dfa00bd917eb9d
tonyg <aj.gianno@gmail.com>
2021-05-07 19:29:16 -0500

    contact form state

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,48 +1,64 @@
 import { Grid, TextField, Typography } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
+import { Form } from 'react-bootstrap';
 import MuiButton from '../../components/Button/MuiButton';
 import resumeData from '../../utils/resumeData';
 import './Contact.css';
 
 const Contact = () => {
+  const [emailSent, setEmailValue] = useState('false');
+
+  const handleSubmit = () => {
+    console.log('hello', emailSent);
+  };
+
   return (
     <>
       {/* Contact */}
       <Grid container spacing={2} className='section pt_45 pb_45'>
         {/* Contact Form */}
-        <Grid item xs={12} lg={7}>
-          <Grid container>
-            <Grid item className='section_title mb_30 top_30'>
-              <span></span>
-              <Typography className='section_typography' variant='h6'>
-                Contact
-              </Typography>
-            </Grid>
+        <Form onSubmit={handleSubmit()}>
+          <Grid item xs={12} lg={7}>
+            <Grid container>
+              <Grid item className='section_title mb_30 top_30'>
+                <span></span>
+                <Typography className='section_typography' variant='h6'>
+                  Contact
+                </Typography>
+              </Grid>
 
-            <Grid item xs={12}>
-              <Grid container spacing={3}>
-                <Grid item xs={12} sm={5}>
-                  <TextField fullWidth name='name' label='Name'></TextField>
-                </Grid>
-                <Grid item xs={12} sm={5}>
-                  <TextField fullWidth name='email' label='E-mail'></TextField>
-                </Grid>
-                <Grid item xs={12} sm={5}>
-                  <TextField
-                    fullWidth
-                    name='message'
-                    label='Message'
-                    multiline
-                    rows={4}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <MuiButton text={'Submit'}></MuiButton>
+              <Grid item xs={12}>
+                <Grid container spacing={3}>
+                  <Grid item xs={12} sm={5}>
+                    <TextField fullWidth name='name' label='Name'></TextField>
+                  </Grid>
+                  <Grid item xs={12} sm={5}>
+                    <TextField
+                      fullWidth
+                      name='email'
+                      label='E-mail'
+                    ></TextField>
+                  </Grid>
+                  <Grid item xs={12} sm={5}>
+                    <TextField
+                      fullWidth
+                      name='message'
+                      label='Message'
+                      multiline
+                      rows={4}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <MuiButton text={'Submit'}></MuiButton>
+                    {/* 
+                    {this.state.emailSent ===
+                      true(<p className='success-msg'>Email Sent</p>)} */}
+                  </Grid>
                 </Grid>
               </Grid>
             </Grid>
           </Grid>
-        </Grid>
+        </Form>
         {/* Contact Information */}
         <Grid item xs={12} lg={5}>
           <Grid container>
