863d8cb6ecc85dcc40fa81e81e4dd24c1b91dcb8
tonyg <aj.gianno@gmail.com>
2021-05-07 22:52:54 -0500

    card borderse

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,64 +1,16 @@
-import { Grid, TextField, Typography } from '@material-ui/core';
-import React, { useState } from 'react';
-import { Form } from 'react-bootstrap';
-import MuiButton from '../../components/Button/MuiButton';
+import { Grid, Typography } from '@material-ui/core';
+// import React, { useState } from 'react';
 import resumeData from '../../utils/resumeData';
 import './Contact.css';
+import ContactForm from './ContactForm';
 
 const Contact = () => {
-  const [emailSent, setEmailValue] = useState('false');
-
-  const handleSubmit = () => {
-    console.log('hello', emailSent);
-  };
-
   return (
     <>
       {/* Contact */}
       <Grid container spacing={2} className='section pt_45 pb_45'>
         {/* Contact Form */}
-        <Form onSubmit={handleSubmit()}>
-          <Grid item xs={12} lg={7}>
-            <Grid container>
-              <Grid item className='section_title mb_30 top_30'>
-                <span></span>
-                <Typography className='section_typography' variant='h6'>
-                  Contact
-                </Typography>
-              </Grid>
-
-              <Grid item xs={12}>
-                <Grid container spacing={3}>
-                  <Grid item xs={12} sm={5}>
-                    <TextField fullWidth name='name' label='Name'></TextField>
-                  </Grid>
-                  <Grid item xs={12} sm={5}>
-                    <TextField
-                      fullWidth
-                      name='email'
-                      label='E-mail'
-                    ></TextField>
-                  </Grid>
-                  <Grid item xs={12} sm={5}>
-                    <TextField
-                      fullWidth
-                      name='message'
-                      label='Message'
-                      multiline
-                      rows={4}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <MuiButton text={'Submit'}></MuiButton>
-                    {/* 
-                    {this.state.emailSent ===
-                      true(<p className='success-msg'>Email Sent</p>)} */}
-                  </Grid>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Form>
+        <ContactForm />
         {/* Contact Information */}
         <Grid item xs={12} lg={5}>
           <Grid container>
@@ -98,12 +50,14 @@ const Contact = () => {
             </Grid>
             <Grid item xs={12}>
               <Grid container className='contactInfo_socialsContainer ml_15'>
-                {Object.keys(resumeData.socials).map((key) => (
-                  <Grid item className='contactInfo_social'>
-                    <a href={resumeData.socials[key].link}>
-                      {resumeData.socials[key].icon}
-                    </a>
-                  </Grid>
+                {Object.keys(resumeData.socials).map((item, index) => (
+                  <div key={index}>
+                    <Grid item className='contactInfo_social'>
+                      <a href={resumeData.socials[item].link}>
+                        {resumeData.socials[item].icon}
+                      </a>
+                    </Grid>
+                  </div>
                 ))}
               </Grid>
             </Grid>
