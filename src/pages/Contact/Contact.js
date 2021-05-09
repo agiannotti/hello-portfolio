994bb4b0e2847d3e632ce0b1c7972a90bf410eb1
Anthony Giannotti <aj.gianno@gmail.com>
2021-05-08 23:26:43 -0500

    extracted form module, Control Module, Input and RadioGroup controls;

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,28 +1,28 @@
-import { Grid, TextField, Typography } from '@material-ui/core';
-import React, { useState } from 'react';
-import { Form } from 'react-bootstrap';
-import MuiButton from '../../components/Button/MuiButton';
-import resumeData from '../../utils/resumeData';
-import './Contact.css';
+import { Grid, TextField, Typography } from "@material-ui/core";
+// import React, { useState } from 'react';
+import { Form } from "react-bootstrap";
+import MuiButton from "../../components/Button/MuiButton";
+import resumeData from "../../utils/resumeData";
+import "./Contact.css";
 
 const Contact = () => {
-  const [emailSent, setEmailValue] = useState('false');
+  // const [emailSent, setEmailValue] = useState('false');
 
   const handleSubmit = () => {
-    console.log('hello', emailSent);
+    // console.log('hello', emailSent);
   };
 
   return (
     <>
       {/* Contact */}
-      <Grid container spacing={2} className='section pt_45 pb_45'>
+      <Grid container spacing={2} className="section pt_45 pb_45">
         {/* Contact Form */}
         <Form onSubmit={handleSubmit()}>
           <Grid item xs={12} lg={7}>
             <Grid container>
-              <Grid item className='section_title mb_30 top_30'>
+              <Grid item className="section_title mb_30 top_30">
                 <span></span>
-                <Typography className='section_typography' variant='h6'>
+                <Typography className="section_typography" variant="h6">
                   Contact
                 </Typography>
               </Grid>
@@ -30,26 +30,26 @@ const Contact = () => {
               <Grid item xs={12}>
                 <Grid container spacing={3}>
                   <Grid item xs={12} sm={5}>
-                    <TextField fullWidth name='name' label='Name'></TextField>
+                    <TextField fullWidth name="name" label="Name"></TextField>
                   </Grid>
                   <Grid item xs={12} sm={5}>
                     <TextField
                       fullWidth
-                      name='email'
-                      label='E-mail'
+                      name="email"
+                      label="E-mail"
                     ></TextField>
                   </Grid>
                   <Grid item xs={12} sm={5}>
                     <TextField
                       fullWidth
-                      name='message'
-                      label='Message'
+                      name="message"
+                      label="Message"
                       multiline
                       rows={4}
                     />
                   </Grid>
                   <Grid item xs={12}>
-                    <MuiButton text={'Submit'}></MuiButton>
+                    <MuiButton text={"Submit"}></MuiButton>
                     {/* 
                     {this.state.emailSent ===
                       true(<p className='success-msg'>Email Sent</p>)} */}
@@ -62,34 +62,34 @@ const Contact = () => {
         {/* Contact Information */}
         <Grid item xs={12} lg={5}>
           <Grid container>
-            <Grid item className='section_title mb_30  ml_15 top_30'>
+            <Grid item className="section_title mb_30  ml_15 top_30">
               <span></span>
-              <Typography className='section_typography ' variant='h6'>
+              <Typography className="section_typography " variant="h6">
                 Contact Information
               </Typography>
             </Grid>
             <Grid item xs={12}>
               <Grid container spacing={2}>
                 <Grid item xs={12}>
-                  <Typography className='contactInfo_item ml_15'>
+                  <Typography className="contactInfo_item ml_15">
                     <span>Name: </span>
                     {resumeData.name}
                   </Typography>
                 </Grid>
                 <Grid item xs={12}>
-                  <Typography className='contactInfo_item ml_15'>
+                  <Typography className="contactInfo_item ml_15">
                     <span>Title: </span>
                     {resumeData.title}
                   </Typography>
                 </Grid>
                 <Grid item xs={12}>
-                  <Typography className='contactInfo_item ml_15'>
+                  <Typography className="contactInfo_item ml_15">
                     <span>Location: </span>
                     {resumeData.address}
                   </Typography>
                 </Grid>
                 <Grid item xs={12}>
-                  <Typography className='contactInfo_item ml_15'>
+                  <Typography className="contactInfo_item ml_15">
                     <span>Phone: </span>
                     {resumeData.phone}
                   </Typography>
@@ -97,9 +97,9 @@ const Contact = () => {
               </Grid>
             </Grid>
             <Grid item xs={12}>
-              <Grid container className='contactInfo_socialsContainer ml_15'>
+              <Grid container className="contactInfo_socialsContainer ml_15">
                 {Object.keys(resumeData.socials).map((key) => (
-                  <Grid item className='contactInfo_social'>
+                  <Grid item className="contactInfo_social">
                     <a href={resumeData.socials[key].link}>
                       {resumeData.socials[key].icon}
                     </a>
